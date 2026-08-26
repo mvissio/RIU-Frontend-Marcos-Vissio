585b507b29f0c1397ed494fba6e002fe272abc0f
Marcos Vissio <marcos.vissio@gmail.com>
2026-08-26 17:03:31 -0300

    feat: use in-memory hero service by default, add provider config for use hero-api with json server

---
+++ src/app/core/services/hero-api.ts
@@ -0,0 +1,45 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { CreateHero, Hero, UpdateHero } from '../models/hero.model';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroApiService {
+  private readonly http = inject(HttpClient);
+
+  private readonly apiUrl = `${environment.apiUrl}/heroes`;
+
+  getAll(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.apiUrl);
+  }
+
+  getById(id: string): Observable<Hero> {
+    return this.http.get<Hero>(`${this.apiUrl}/${id}`);
+  }
+
+  searchByName(query: string): Observable<Hero[]> {
+    // name_like Es requerido para la busqueda con JsonServer
+    const searchTerm = query.trim();
+    return this.http.get<Hero[]>(`${this.apiUrl}?name_like=${searchTerm}`);
+  }
+
+  create(hero: CreateHero): Observable<Hero> {
+    return this.http.post<Hero>(this.apiUrl, {
+      ...hero,
+      createdAt: new Date(),
+    });
+  }
+
+  update(id: string, changes: UpdateHero): Observable<Hero | null> {
+    return this.http.put<Hero>(`${this.apiUrl}/${id}`, changes);
+  }
+
+  delete(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
+  useMockApi: false,
   apiUrl: 'http://localhost:3000',
 };
